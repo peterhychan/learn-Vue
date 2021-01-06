051b54f93ac9ed8e027e1fcf145409974ba638fc
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2021-01-05 16:36:31 -0800

    first commit

---
+++ reaction-timer/README.md
@@ -0,0 +1,21 @@
+# reaction-timer
+
+The application is now deployed on [now.sh](https://reaction-timer-rose.vercel.app).
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time: {{score}} ms</p>
+  <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data(){
+    return{
+      rank: null
+    }
+  },
+  mounted(){
+    if(this.score < 250){
+      this.rank = "S Class"
+    }else if(this.score <400){
+      this.rank = "A Class"
+    }else{
+      this.rank = "B Class"
+    }
+  }
+}
+</script>
+
+<style>
+.rank{
+  font-size: 1.5em;
+  color: #00ffaa;
+  font-weight: bold;
+}
+</style>
